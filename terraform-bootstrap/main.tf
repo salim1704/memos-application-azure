4444a23294ca591d5f3962ccc4a6449aa05dde60
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2026-02-22 16:46:25 +0000

    Terraform bootstrap - main.tf to create backend blob for state

---
+++ terraform-bootstrap/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  resource_provider_registrations = "none"
+}
+
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "${var.prefix}-tfstate-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                            = "${var.prefix}${random_string.resource_code.result}tfstate"
+  resource_group_name             = azurerm_resource_group.tfstate.name
+  location                        = azurerm_resource_group.tfstate.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+
+  blob_properties {
+    versioning_enabled = true
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
