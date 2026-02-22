0af9f51f7771fe866233798814051a6fd731d29a
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2026-02-22 17:03:45 +0000

    Terraform - aws required provider added

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.61.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
 }
 
 provider "aws" {
