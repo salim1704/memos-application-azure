4a1f9a2cd30a2a6a91669ea29a3f79f096ec1646
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2026-02-22 16:57:29 +0000

    Terraform - aws and azure providers

---
+++ terraform/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.61.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
